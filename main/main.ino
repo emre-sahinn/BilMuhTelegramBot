a07141b5c2e77588880cb32e9d4daedbd8776740
ARXINO <arxtechnologies@gmail.com>
2020-03-15 16:09:32 +0300

    v1.1
    Firebase realtime database and HTTPS data optimization added.

--- main/main.ino
+++ main/main.ino
@@ -1,218 +1,122 @@
-#include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
 #include <UniversalTelegramBot.h>
-#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
 
-const char *ssid = "";
-const char *password = "";
+#define FIREBASE_HOST "bilmuhbot.firebaseio.com"
+#define FIREBASE_AUTH "uDCLDq4WNrwV9McjLNr1KCaTImdYvpYKNYnWNX3a"
+#define WIFI_SSID "internetev"
+#define WIFI_PASSWORD "emre.1912"
 
-String ChatID = "";
-
-#define BOTtoken ""
+#define ChatID  "872893196"
+#define logChatID  "872893196"
+#define BOTtoken "1141708876:AAEzVcWR0zKuGyDAFyiG2bQROV5u8u4fGxU"
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 
 //String test_photo_url = "https://www.arduino.cc/en/uploads/Trademark/ArduinoCommunityLogo.png";
-
-String oldResult;
-String msg;
 
 const char *host = "api.thingspeak.com";
 const int httpsPort = 443;  //HTTPS= 443 and HTTP = 80
 
 const char fingerprint[] PROGMEM = "F9 C2 65 6C F9 Ef 7F 66 8B F7 35 FE 15 EA 82 9F 5F 55 54 3E";
 
+String msg = "emre sahin";
+
 void setup() {
-  EEPROM.begin(50);
+  Serial.begin(115200);
   client.setInsecure();
-  //bot._debug = true;
-  delay(1000);
-  //Serial.begin(115200);
   WiFi.mode(WIFI_OFF);
   delay(1000);
   WiFi.mode(WIFI_STA);
-
-  WiFi.begin(ssid, password);
-  //Serial.println("");
-
-  //Serial.print("Connecting");
-  // Wait for connection
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
     delay(500);
-    //Serial.print(".");
   }
-}
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
 
-void handleNewMessages(int numNewMessages) {
-  for (int i = 0; i < numNewMessages; i++) {
-    String chat_id = String(bot.messages[i].chat_id);
-    String text = bot.messages[i].text;
-
-    String from_name = bot.messages[i].from_name;
-    if (from_name == "") from_name = "Efendim";
-
-    /*if (text == "/get_test_photo") {
-      bot.sendPhoto(chat_id, test_photo_url, "Caption is optional, you may not use photo caption");
-      }*/
-
-    if (text == "/komutlar") {
-      String welcome = "Merhaba " + from_name + ".\n";
-      welcome += "Ben ESP8266 mikrokontrolcüsü üzerinde çalışan ve bilmuh adresinden duyuruları çeken bir botum.\n";
-      welcome += "Yazılım: Emre ŞAHİN, Numan KILIÇ, Fatih CÜRE\n";
-      welcome += "github.com/ARXINO/BilMuhTelegramBot || Versiyon 1.1 || 04.03.2020\n\n";
-      welcome += "Komutlara yanıt en geç 2 dk içinde gelir.\n";
-      welcome += "Komutlar:\n";
-      welcome += "/komutlar : komutları listeler\n";
-      //welcome += "/foto     : duyuruları .jpeg olarak atar\n";
-      welcome += "/reset    : sunucuya reset atar\n";
-      welcome += "/butunduyurular : bütün duyuruları çeker\n";
-      welcome += "/kapat    : mesaj atmayı kapatır\n";
-      bot.sendMessage(ChatID, welcome, "");
-    }
-
-    if (text == "/reset") {
-      ChatID = "";
-      String welcome = "Reset atılıyor..\n";
-      bot.sendMessage(ChatID, welcome, "");
-      ESP.reset();
-    }
-
-    if (text == "/butunduyurular") {
-      String welcome = "Merhaba " + from_name + ".\n";
-      welcome += "Henüz bunun kodu yazılmadı :(\n";
-      bot.sendMessage(ChatID, welcome, "");
-    }
-
-    if (text == "/kapat") {
-      String welcome = "Kapatılıyor..\n";
-      bot.sendMessage(ChatID, welcome, "");
-      ChatID = "";
-    }
-
-    //KOMUTLARIN KODUNU YAZ!
-
-  }
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
 void loop() {
-  bot.sendMessage("", "OK", "");
 
-  client.setFingerprint(fingerprint);
-  //client.setTimeout(15000); // 15 Seconds
-  delay(1000);
-
-  //Serial.print("HTTPS Connecting");
-  int r = 0; //retry counter
+  //client.setFingerprint("BB DC 45 2A 07 E3 4A 71 33 40 32 DA BE 81 F7 72 6F 4A 2B 6B");
+  Serial.print("bot durum:"); Serial.println(bot.sendMessage(logChatID, "OK..", ""));
+  //client.setFingerprint(fingerprint);
+  int8_t r = 0; //retry counter
   while ((!client.connect(host, httpsPort)) && (r < 30)) {
     delay(100);
     //Serial.print(".");
     r++;
   }
-  /*if (r == 30) {
-    //Serial.println("Connection failed");
-    }
-    else {
-    //Serial.println("Connected to web");
-    }*/
-  String Link;
 
-  //GET Data
-  Link = "/apps/thinghttp/send_request?api_key=WTPIX8RFT54YZAZE";// /apps/thinghttp/send_request?api_key=AGK10O9MPFY48XCS
-
-  //Serial.print("requesting URL: ");
-  //Serial.println(host + Link);
-
-  client.print(String("GET ") + Link + " HTTP/1.1\r\n" +
+  client.print(String("GET ") + "/apps/thinghttp/send_request?api_key=AGK10O9MPFY48XCS" + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "Connection: close\r\n\r\n");
-  //Serial.println("request sent");
+  delay(1000);
+  String line;
+  int firstIndex = -1;
+  int lastIndex = -1;
+  firstIndex = line.indexOf('<', firstIndex + 1);
+  lastIndex = line.indexOf('>', lastIndex + 1);
 
-  //int say = 0;
-  String line = "";
-  /*Serial.println("reply was:");
-    Serial.println("==========");*/
-  line = client.readString();
-  //Serial.println(line); //Print response
-  /*while (httpsClient.available()) {
-    line = httpsClient.readStringUntil('\n');  //Read Line by Line
-    Serial.println(line); //Print response
-    }*/
-  //Serial.println("==========");
-  //Serial.println("closing connection");
-
-
-  client.setFingerprint("BB DC 45 2A 07 E3 4A 71 33 40 32 DA BE 81 F7 72 6F 4A 2B 6B");
-  int numNewMessages = 1;//bot.getUpdates(bot.last_message_received + 1);
-  while (numNewMessages) {
-    //Serial.println("got response");
-    handleNewMessages(numNewMessages);
-    numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+  for (uint8_t i; i < 20; i++) {
+    line = client.readStringUntil('\n');  //Read Line by Line
+    //Serial.print(i); Serial.println(line); //Print response
   }
-  /* HTML PARSE OPERATIONS - BEGIN */
-  if (line != oldResult && line != "") {
-    //Serial.println("Yeni Duyuru Var!");
-    oldResult = line;
-    int firstIndex = -1;
-    int lastIndex = -1;
-    //msg_1
-    for (int i = 0; i < 18; i++) {
-      firstIndex = line.indexOf('<', firstIndex + 1);
-    }
-    for (int i = 0; i < 17; i++) {
-      lastIndex = line.indexOf('>', lastIndex + 1);
-    }
-    String temp = "";
-    msg = line.substring(lastIndex, firstIndex);
-    temp = msg + " duyurusu yayınlanmıştır. Ayrıntısını görmek için https://bilmuh.ege.edu.tr/ adresine bakınız.";
-    String oldMsg = "";
-    for (byte i = 0; i < 50; i++) {
-      oldMsg += char(EEPROM.read(i));
-      delay(100);
-    }
-    //Serial.print("OLD MESSAGE: ");Serial.println(oldMsg);
-    //Serial.print("msg.substring(0,50): ");Serial.println(oldMsg);
 
-    if (msg.substring(0, 50) != oldMsg && msg != "") {
-      for (byte i = 0; i < 50; i++) {
-        EEPROM.write(i, temp.charAt(i));
-        delay(100);
-      }
-      EEPROM.commit(); //returns true when success
-      //Serial.println("New Message 1");
-      //Serial.println(msg);
-      //oldMsg = msg;
-      //Serial.print("Durum 1: ");
-      bot.sendMessage(ChatID, temp, "");//kontrol ekle while olan
-      delay(1000);
-      //Serial.println(1);
-    }
-    //end of msg_1
-
-    //msg_2
-    /*
-      for (int i = 0; i < 19; i++) {
-      firstIndex = line.indexOf('<', firstIndex + 1);
-      }
-      for (int i = 0; i < 19; i++) {
-      lastIndex = line.indexOf('>', lastIndex + 1);
-      }
-      msg2 = line.substring(lastIndex, firstIndex);
-      temp = msg2 + " duyurusu yayınlanmıştır. Ayrıntısını görmek için https://bilmuh.ege.edu.tr/ adresine bakınız.";
-      if (msg2 != oldMsg2 && msg2 != msg && msg2 != "") {
-      //Serial.println("New Message 2");
-      //Serial.println(msg2);
-      oldMsg2 = msg2;
-      //Serial.print("Durum 2: ");
-      bot.sendMessage(ChatID, temp, "");
-      delay(1000);
-      //Serial.println(1);
-      }
-    */
-    //end of msg_2
+  //msg_1
+  for (int i = 0; i < 2; i++) {
+    firstIndex = line.indexOf('<', firstIndex + 1);
   }
-  /* HTML PARSE OPERATIONS - END */
+  for (int i = 0; i < 1; i++) {
+    lastIndex = line.indexOf('>', lastIndex + 1);
+  }
+  msg = line.substring(lastIndex + 1, firstIndex);
 
-  delay(60000);  //GET Data at every 60 seconds
+  Serial.println("***");
+  Serial.println(msg);
+  Serial.println("***");
+  //Convert special characters
+  msg.replace("Ç", "C");
+  msg.replace("İ", "I");
+  msg.replace("Ü", "U");
+  msg.replace("Ğ", "G");
+  msg.replace("Ö", "O");
+  msg.replace("Ş", "S");
+  msg.replace("ı", "i");
+  msg.replace("ç", "c");
+  msg.replace("ü", "u");
+  msg.replace("ğ", "g");
+  msg.replace("ö", "o");
+  msg.replace("ş", "s");
+  msg.replace("⸮", " ");
+  /////////////////////
+  Serial.println(msg);
+  String f_data = Firebase.getString("Duyuru1");
+  Serial.println("???");
+  Serial.println(f_data);
+  Serial.println(msg);
+  Serial.println(f_data != msg);
+  Serial.println("???");
+  if (f_data != "" && msg != "" && f_data != msg) {
+    Serial.println(f_data);
+    Firebase.setString("Duyuru1", msg);
+    if (Firebase.failed()) {
+      Serial.print("message failed:"); Serial.println(Firebase.error());
+      bot.sendMessage(logChatID, Firebase.error(), "");
+    }
+    delay(3000);
+    Serial.print("bot durum msg:"); Serial.println(bot.sendMessage(ChatID, msg + " duyurusu yayinlanmistir. Ayrıntısını görmek için https://bilmuh.ege.edu.tr/ adresine bakınız.", ""));
+    delay(3000);
+  }
+
+  delay(300000);
 }
